82c211bb225891ace299c532a02493d251cf81e1
Shresth Rajput <rajshresth2@gmail.com>
2026-02-03 11:03:44 +0530

    useRef Part-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Demo2 from "./Topic-9 useRef Hook/Demo2";
+import Demo3 from "./Topic-9 useRef Hook/Demo3";
 
 const App = () => {
   return (
     <div id="app-component">
       {/* <Demo /> */}
-      <Demo2 />
+      {/* <Demo2 /> */}
+      <Demo3/>
     </div>
   );
 };
---
+++ src/Topic-9 useRef Hook/Demo3.jsx
@@ -0,0 +1,30 @@
+import {useRef,useState} from 'react'
+
+const Demo3 = () => {
+    const [count,setCount] = useState(0);
+
+    const z = useRef(null)
+    // z = { current: null }
+
+    const handleCount = () => {
+        setCount(count + 1);
+        console.log("useRef-z:",z.current);
+        z.current.style.color="red";
+    }
+
+  return (
+    <div>
+        <h1>Demo3 Component</h1>
+        <h2>Count: {count}</h2>
+        <button onClick={handleCount}>click</button>
+        <input
+         type="text"
+         placeholder='Enter fullname'
+         name='fullname'
+         ref={z}
+         />
+    </div>
+  )
+}
+
+export default Demo3;
