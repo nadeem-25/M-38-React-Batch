0a92622c76f86ba5d7e12d37e336bacf507de1e1
Shresth Rajput <rajshresth2@gmail.com>
2026-02-05 10:49:26 +0530

    useEffect Part-2

--- src/Topic-10 useEffect Hook/Demo2.jsx
+++ src/Topic-10 useEffect Hook/Demo2.jsx
@@ -1,29 +1,30 @@
 import { useState, useEffect } from "react";
 
 const Demo2 = () => {
-  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0); // count1 = 1
+  const [count2, setCount2] = useState(0); // count2 = 10
 
   useEffect(() => {
-    console.log("useEffect Call");
+    console.log("useEffect");
 
     return () => {
-      console.log("Cleanup Call");
-    };
-  }, []);
+      console.log("Cleanup")
+    }
+  }, [count2]);
 
-  console.log("Demo2 render");
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
+  console.log("Demo2 Render");
 
   return (
     <div>
       <h1>Demo2 Component</h1>
-      <h2>Count: {count}</h2>
-      <button onClick={handleClick}>Click</button>
+      <h2>Count1: {count1}</h2>
+      <button onClick={()=> setCount1(count1+1)}>Click</button>
+
+      <h2>Count2: {count2}</h2>
+      <button onClick={()=> setCount2(count2 + 10)}>Click</button>
     </div>
   );
 };
 
 export default Demo2;
+
