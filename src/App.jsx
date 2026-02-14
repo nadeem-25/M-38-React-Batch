3d1274ea4ff26030b016c3e663fbf4497917a05c
nadeem-25 <nadeem25official@gmail.com>
2026-02-15 01:08:05 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Parent from "./Topic-14 React Memo/Parent";
+//import Parent from "./Topic-14 React Memo/Parent";
+//import Parent from "./Topic-13 useEffect stale case/Parent";
+import Container from "./Topic-12 useEffect Api call and shimmer ui/Container"
 
 const App = () => {
   return (
     <div id="app-component">
-     <Parent/>
+     <Container/>
     </div>
   )
 }
